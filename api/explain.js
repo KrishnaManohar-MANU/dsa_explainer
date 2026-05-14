68059ce2b8ae39e678cf1dac1ebfdd1bf5dd758d
Krishna Manohar <krishnamanohar0107@gmail.com>
2026-05-14 15:48:19 +0530

    understood a colon and equal symbol are different

--- api/explain.js
+++ api/explain.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model = "openrouter/auto",
+        model : "openrouter/auto",
         response_format: { type: "json_object" },
         messages: [
           {
